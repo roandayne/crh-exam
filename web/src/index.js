8099f84d590e9c82aec7cd303a2566c96248cdf1
Roan Dayne Dino <roandaynedenaerys@gmail.com>
2022-12-16 12:17:06 +0800

    add router

--- web/src/index.js
+++ web/src/index.js
@@ -2,10 +2,26 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
+import Dashboard from './Dashboard'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/dashboard",
+    element: <Dashboard />,
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 )
